cd7c6b7a599c67fb4ab3776ef735025b00143ba3
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-03-20 15:42:39 -0400

    2nd account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "appleshampooid+aftdev@gmail.com"
+    AccountName               = "dev-aft"
+    ManagedOrganizationalUnit = "Dev (ou-17c9-7ufyjtw7)"
+    SSOUserEmail              = "appleshampooid+aftdev@gmail.com"
+    SSOUserFirstName          = "Dev"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
